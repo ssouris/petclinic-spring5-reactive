e318c13c3446e823d25d77693bd33efc72ef35e3
ssouris <ssouris@thousandeyes.com>
2017-05-24 11:24:30 +0100

    Experiment with WebClient a little bit

--- src/main/kotlin/com/yetanotherdevblog/MyBlogPostHandler.kt
+++ src/main/kotlin/com/yetanotherdevblog/MyBlogPostHandler.kt
@@ -1,6 +1,8 @@
 package com.yetanotherdevblog
 
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse.ok
 import org.springframework.web.reactive.function.server.body
@@ -14,6 +16,15 @@ class MyBlogPostHandler(val myBlogPostRepo: MyBlogPostRepo) {
 
     fun matchClock(serverRequest: ServerRequest) = ok()
             .textEventStream()
-            .body(Flux.interval(Duration.ofMillis(200)).map { "Second ${it}" }, String::class.java)
+            .body(Flux.interval(Duration.ofMillis(200)).map { "Second $it" }, String::class.java)
+
+    fun experimentingWithWebClient() {
+        val client = WebClient.create("http://localhost:8081")
+                .get()
+                .uri("/")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(String::class.java)
+    }
 
 }
