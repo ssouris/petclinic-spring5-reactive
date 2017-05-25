13ba4f37b73cd1a443f61a7cbfd7901c484aadd5
ssouris <ssouris@thousandeyes.com>
2017-05-25 16:02:54 +0100

    Commiting some really bad kotlin usage....

--- src/main/kotlin/com/yetanotherdevblog/Extensions.kt
+++ src/main/kotlin/com/yetanotherdevblog/Extensions.kt
@@ -11,3 +11,31 @@ inline fun <reified T : Any> ReactiveMongoOperations.findAll(): Flux<T> =
 fun BodyBuilder.json() = contentType(APPLICATION_JSON_UTF8)
 
 fun BodyBuilder.textEventStream() = contentType(TEXT_EVENT_STREAM)
+
+// port StringUtils to Kotlin method extensions
+fun CharSequence.isEmpty() = this.length == 0
+fun CharSequence.isNotEmpty() = !this.isEmpty()
+
+
+
+// BAD THINGS - DO NOT DO THIS
+
+// method names can be free text
+infix fun CharSequence.`are you ok man`(arg1 : String)
+        = println("Yeap")
+
+fun main1(args: Array<String>) {
+    // with infix methods can skip the '(', ')'
+    "Stathis Souris " `are you ok man` "?"
+    println("st" + "asd")
+}
+
+fun String.plus(other: Any?)= when (other) {
+        is String -> this.toUpperCase().plus(other.toUpperCase())
+        else -> throw IllegalArgumentException()
+}
+
+//fun main(args: Array<String>) {
+    //    "Stathis Souris " `are you ok man` "?"
+//    println("st" + "asd")
+//}
