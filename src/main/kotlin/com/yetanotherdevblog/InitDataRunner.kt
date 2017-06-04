99dca1a0c5c2541528ef9ce764c04e5a1fff8e18
ssouris <ssouris@thousandeyes.com>
2017-06-04 21:24:49 +0100

    Add extension method for Flux, Mono subscribe with only onComplete callback.

--- src/main/kotlin/com/yetanotherdevblog/InitDataRunner.kt
+++ src/main/kotlin/com/yetanotherdevblog/InitDataRunner.kt
@@ -12,6 +12,8 @@ import com.yetanotherdevblog.petclinic.repositories.SpecialityRepository
 import com.yetanotherdevblog.petclinic.repositories.VetRepository
 import org.springframework.boot.CommandLineRunner
 import org.springframework.stereotype.Component
+import reactor.core.Disposable
+import reactor.core.publisher.Mono
 import java.time.LocalDate
 import java.util.UUID
 
@@ -27,22 +29,22 @@ class InitDataRunner(val petTypeRepository: PetTypeRepository,
         val ownerId = UUID.fromString("5bead0d3-cd7b-41e5-b064-09f48e5e6a08").toString()
         val dogId = UUID.randomUUID().toString()
 
-        petTypeRepository.deleteAll().subscribe(null, null, {
+        petTypeRepository.deleteAll().subscribeOnComplete {
             val petTypes = listOf("cat", "lizard", "snake", "bird", "hamster", "dog")
                     .map { if (it == "dog") PetType(name=it, id = dogId) else PetType(name = it) }
             petTypeRepository.saveAll(petTypes)
                     .subscribe( null, null, { println("Added  PetTypes") })
-        })
+        }
 
 
-        specialityRepository.deleteAll().subscribe(null, null, {
+        specialityRepository.deleteAll().subscribeOnComplete {
             val specialities = listOf("radiology", "dentistry", "surgery")
                     .map {Speciality(name = it)}
             specialityRepository.saveAll(specialities)
                     .subscribe( null, null, { println("Added  Specialities") })
-        })
+        }
 
-        vetRepository.deleteAll().subscribe(null, null, {
+        vetRepository.deleteAll().subscribeOnComplete {
             vetRepository.saveAll(listOf(
                     Vet(firstName = "James", lastName="Carter"),
                     Vet(firstName = "Helen", lastName="Leary", specialities = setOf("radiology")),
@@ -51,28 +53,32 @@ class InitDataRunner(val petTypeRepository: PetTypeRepository,
                     Vet(firstName = "Henry", lastName="Stevens", specialities = setOf("radiology")),
                     Vet(firstName = "Sharon", lastName="Jenkins")))
                     .subscribe( null, null, { println("Added  Vets") })
+        }
 
-        })
-
-        ownersRepository.deleteAll().subscribe(null, null, {
+        ownersRepository.deleteAll().subscribeOnComplete {
             ownersRepository.saveAll(listOf(
                     Owner(firstName = "James", lastName="Carter",
                             telephone = "123", address = "123", city = "asd",
                             id = ownerId)))
                     .subscribe( null, null, { println("Added  Owners") })
+        }
 
-        })
-
-        petRepository.deleteAll().subscribe(null, null, {
+        petRepository.deleteAll().subscribeOnComplete {
             petRepository.saveAll(listOf(Pet(
                     name = "Some name",
                     birthDate = LocalDate.now(),
                     type = dogId,
                     owner = ownerId)))
                     .subscribe( null, null, { println("Added Pets") })
+        }
 
-        })
+    }
 
+    /**
+     * Subscribe onComplete only. Used by db populators.
+     */
+    private fun <T> Mono<T>.subscribeOnComplete(completeConsumer: () -> Unit) : Disposable {
+        return this.subscribe(null, null, completeConsumer)
     }
 
 }
